a98fabdf9989134ca7799288db09dc8221c85369
KrevetukasLT <88585435+HungryMuttis@users.noreply.github.com>
2025-11-20 19:51:32 +0200

    fixed config and made a workflow

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,11 @@ import {defineConfig} from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  envDir: '../',
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-      },
+    base: 'DiscrodActivity-Wordle-Frontend',
+    envDir: '/',
+    server: {
+        hmr: {
+            clientPort: 443,
+        }
     },
-    hmr: {
-      clientPort: 443,
-    },
-    allowedHosts: [
-        'responding-arnold-myrtle-late.trycloudflare.com'
-    ]
-  },
 });
