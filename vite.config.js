561a6b60d2a38c9e9a587bf3d85217212262cb3c
KrevetukasLT <88585435+HungryMuttis@users.noreply.github.com>
2025-11-20 20:07:16 +0200

    what am I doing even?

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import {defineConfig} from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: 'DiscrodActivity-Wordle-Frontend',
+    base: '/DiscrodActivity-Wordle-Frontend',
     envDir: '/',
     server: {
         hmr: {
